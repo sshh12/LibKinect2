e131ed3834829dc6b19adec09f0d054ff8a8de34
Shrivu Shankar <shrivu1122@gmail.com>
2019-06-08 20:38:26 -0500

    Added IR and .disconnect()

--- Kinect2-API/Kinect2-API/Kinect2-API.cpp
+++ Kinect2-API/Kinect2-API/Kinect2-API.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include <Kinect.h>
 #include <cstring>
+#include <iostream>
 
 
 #define EXPORTFUNC extern "C" __declspec(dllexport)
@@ -8,68 +9,115 @@
 
 #define F_SENSOR_COLOR 0x00000001
 #define F_SENSOR_DEPTH 0x00000010
-#define COLOR_WIDTH 1920
-#define COLOR_HEIGHT 1080
+#define F_SENSOR_IR    0x00000100
+#define COLOR_WIDTH    1920
+#define COLOR_HEIGHT   1080
 #define COLOR_CHANNELS 4
-#define DEPTH_WIDTH 512
-#define DEPTH_HEIGHT 424
+#define DEPTH_WIDTH    512
+#define DEPTH_HEIGHT   424
+#define IR_WIDTH       512
+#define IR_HEIGHT      424
 
 
+int sensors = 0;
 IKinectSensor* sensor;
-IColorFrameReader* reader_color;
-IDepthFrameReader* reader_depth;
+IMultiSourceFrameReader* multi_reader;
+
+UINT8 buffer_color[COLOR_WIDTH * COLOR_HEIGHT * COLOR_CHANNELS];
+UINT16 buffer_depth[DEPTH_WIDTH * DEPTH_HEIGHT];
+UINT16 buffer_ir[IR_WIDTH * IR_HEIGHT];
 
 
 EXPORTFUNC bool init_kinect(int sensor_flags) {
 	if (!sensor_flags || FAILED(GetDefaultKinectSensor(&sensor))) {
 		return false;
 	}
+	sensors = sensor_flags;
 	if (sensor) {
+		int source_types = 0; 
+		if (sensors & F_SENSOR_COLOR) {
+			source_types |= FrameSourceTypes::FrameSourceTypes_Color;
+		}
+		if (sensors & F_SENSOR_DEPTH) {
+			source_types |= FrameSourceTypes::FrameSourceTypes_Depth;
+		}
+		if (sensors & F_SENSOR_IR) {
+			source_types |= FrameSourceTypes::FrameSourceTypes_Infrared;
+		}
 		sensor->Open();
-		if (sensor_flags & F_SENSOR_COLOR) {
-			IColorFrameSource* source_color = NULL;
-			sensor->get_ColorFrameSource(&source_color);
-			source_color->OpenReader(&reader_color);
-			if (source_color) {
-				source_color->Release();
-				source_color = NULL;
-			}
+		sensor->OpenMultiSourceFrameReader(source_types, &multi_reader);
+		if (multi_reader) {
+			return true;
 		}
-		if (sensor_flags & F_SENSOR_DEPTH) {
-			IDepthFrameSource* source_depth = NULL;
-			sensor->get_DepthFrameSource(&source_depth);
-			source_depth->OpenReader(&reader_depth);
-			if (source_depth) {
-				source_depth->Release();
-				source_depth = NULL;
-			}
-		}
-		return true;
-	} else {
+	}
+	return false;
+}
+
+
+EXPORTFUNC void close_kinect() {
+	sensor->Close();
+	sensor->Release();
+}
+
+
+EXPORTFUNC bool read_sensors() {
+	IMultiSourceFrame* frame = NULL;
+	if (FAILED(multi_reader->AcquireLatestFrame(&frame))) {
 		return false;
 	}
+	if (sensors & F_SENSOR_COLOR) {
+		IColorFrame* frame_color;
+		IColorFrameReference* frameref_color = NULL;
+		frame->get_ColorFrameReference(&frameref_color);
+		frameref_color->AcquireFrame(&frame_color);
+		if (frameref_color) frameref_color->Release();
+		if (frame_color) {
+			frame_color->CopyConvertedFrameDataToArray(COLOR_WIDTH * COLOR_HEIGHT * COLOR_CHANNELS, buffer_color, ColorImageFormat_Rgba);
+			frame_color->Release();
+		}
+	}
+	if (sensors & F_SENSOR_DEPTH) {
+		IDepthFrame* frame_depth;
+		IDepthFrameReference* frameref_depth = NULL;
+		frame->get_DepthFrameReference(&frameref_depth);
+		frameref_depth->AcquireFrame(&frame_depth);
+		if (frameref_depth) frameref_depth->Release();
+		if (frame_depth) {
+			frame_depth->CopyFrameDataToArray(DEPTH_WIDTH * DEPTH_HEIGHT, buffer_depth);
+			frame_depth->Release();
+		}
+	}
+	if (sensors & F_SENSOR_IR) {
+		IInfraredFrame * frame_ir;
+		IInfraredFrameReference* frameref_ir = NULL;
+		frame->get_InfraredFrameReference(&frameref_ir);
+		frameref_ir->AcquireFrame(&frame_ir);
+		if (frameref_ir) frameref_ir->Release();
+		if (frame_ir) {
+			frame_ir->CopyFrameDataToArray(IR_WIDTH * IR_HEIGHT, buffer_ir);
+			frame_ir->Release();
+		}
+	}
+	return true;
 }
 
 
-EXPORTFUNC bool get_color_data(UINT8* array) {
-	IColorFrame* frame = NULL;
-	bool success = false;
-	if (SUCCEEDED(reader_color->AcquireLatestFrame(&frame))) {
-		frame->CopyConvertedFrameDataToArray(COLOR_WIDTH * COLOR_HEIGHT * COLOR_CHANNELS, array, ColorImageFormat_Bgra);
-		success = true;
-	}
-	if (frame) frame->Release();
-	return success;
+EXPORTFUNC bool get_color_data(UINT8* array, bool do_read) {
+	if (do_read) read_sensors();
+	memcpy(array, buffer_color, COLOR_WIDTH * COLOR_HEIGHT * COLOR_CHANNELS * sizeof(UINT8));
+	return true;
 }
 
 
-EXPORTFUNC bool get_depth_data(UINT16* array) {
-	IDepthFrame* frame = NULL;
-	bool success = false;
-	if (SUCCEEDED(reader_depth->AcquireLatestFrame(&frame))) {
-		frame->CopyFrameDataToArray(DEPTH_WIDTH * DEPTH_HEIGHT, array);
-		success = true;
-	}
-	if (frame) frame->Release();
-	return success;
+EXPORTFUNC bool get_ir_data(UINT16* array, bool do_read) {
+	if (do_read) read_sensors();
+	memcpy(array, buffer_ir, IR_WIDTH * IR_HEIGHT * sizeof(UINT16));
+	return true;
+}
+
+
+EXPORTFUNC bool get_depth_data(UINT16* array, bool do_read) {
+	if (do_read) read_sensors();
+	memcpy(array, buffer_depth, DEPTH_WIDTH * DEPTH_HEIGHT * sizeof(UINT16));
+	return true;
 }
--- libkinect2/kinect.py
+++ libkinect2/kinect.py
@@ -2,48 +2,68 @@ from pkg_resources import resource_filename
 import numpy as np
 import ctypes
 
+
 F_SENSOR_COLOR = 0x00000001
 F_SENSOR_DEPTH = 0x00000010
-
-COLOR_WIDTH = 1920
-COLOR_HEIGHT = 1080
+F_SENSOR_IR    = 0x00000100
+COLOR_WIDTH    = 1920
+COLOR_HEIGHT   = 1080
 COLOR_CHANNELS = 4
-DEPTH_WIDTH = 512
-DEPTH_HEIGHT = 424
+DEPTH_WIDTH    = 512
+DEPTH_HEIGHT   = 424
+IR_WIDTH       = 512
+IR_HEIGHT      = 424
 
 kinectDLL = ctypes.cdll.LoadLibrary(resource_filename(__name__, 'data/Kinect2-API.dll'))
 
 kinectDLL.init_kinect.argtypes = [ctypes.c_int]
 kinectDLL.init_kinect.restype = ctypes.c_bool
 
-kinectDLL.get_color_data.argtypes = [np.ctypeslib.ndpointer(dtype=np.uint8)]
+kinectDLL.close_kinect.argtypes = []
+kinectDLL.close_kinect.restype = None
+
+kinectDLL.get_color_data.argtypes = [np.ctypeslib.ndpointer(dtype=np.uint8), ctypes.c_bool]
 kinectDLL.get_color_data.restype = ctypes.c_bool
 
-kinectDLL.get_depth_data.argtypes = [np.ctypeslib.ndpointer(dtype=np.uint16)]
+kinectDLL.get_ir_data.argtypes = [np.ctypeslib.ndpointer(dtype=np.uint16), ctypes.c_bool]
+kinectDLL.get_ir_data.restype = ctypes.c_bool
+
+kinectDLL.get_depth_data.argtypes = [np.ctypeslib.ndpointer(dtype=np.uint16), ctypes.c_bool]
 kinectDLL.get_depth_data.restype = ctypes.c_bool
 
 class Kinect2:
 
-    def __init__(self, use_color=True, use_depth=True):
+    def __init__(self, use_color=True, use_depth=True, use_ir=True):
         self.sensor_flags = 0
         if use_color:
             self.sensor_flags |= F_SENSOR_COLOR
         if use_depth:
             self.sensor_flags |= F_SENSOR_DEPTH
+        if use_ir:
+            self.sensor_flags |= F_SENSOR_IR
 
     def connect(self):
         if not kinectDLL.init_kinect(self.sensor_flags):
             raise IOError('Unable to init Kinect2 Sensor.')
         return True
 
+    def disconnect(self):
+        kinectDLL.close_kinect()
+
     def get_color_image(self):
         color_ary = np.zeros((COLOR_HEIGHT, COLOR_WIDTH, COLOR_CHANNELS), np.uint8)
-        if kinectDLL.get_color_data(color_ary):
+        if kinectDLL.get_color_data(color_ary, True):
             return color_ary
         return None
 
-    def get_depth_image(self):
+    def get_ir_image(self):
+        ir_ary = np.zeros((IR_HEIGHT, IR_WIDTH, 1), np.uint16)
+        if kinectDLL.get_ir_data(ir_ary, True):
+            return ir_ary
+        return None
+
+    def get_depth_map(self):
         depth_ary = np.zeros((DEPTH_HEIGHT, DEPTH_WIDTH, 1), np.uint16)
-        if kinectDLL.get_depth_data(depth_ary):
+        if kinectDLL.get_depth_data(depth_ary, True):
             return depth_ary
         return None
